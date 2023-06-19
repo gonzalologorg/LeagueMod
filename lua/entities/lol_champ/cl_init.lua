b051e74b7c0a301c82d30a5400d633dac4b63782
Gonzalolog <gonzalolog@gmail.com>
2023-06-19 04:29:35 -0300

    Added champion base

---
+++ lua/entities/lol_champ/cl_init.lua
@@ -0,0 +1,11 @@
+include("shared.lua")
+ENT.RenderGroup = RENDERGROUP_BOTH
+
+function ENT:Initialize()
+end
+
+function ENT:Draw()
+end
+
+function ENT:DrawTranslucent()
+end
---
+++ lua/entities/lol_champ/shared.lua
@@ -0,0 +1,11 @@
+
+ENT.Base = "base_anim"
+ENT.Type = "anim"
+
+ENT.PrintName = "League Base"
+ENT.Category = "League of Legends"
+
+ENT.Spawnable = false
+
+function ENT:SetupDataTables()
+end
