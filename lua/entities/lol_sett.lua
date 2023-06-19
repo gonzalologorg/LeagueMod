959809608ff7bc2996b023c86bc76d5ccb5f6145
Gonzalolog <gonzalolog@gmail.com>
2023-06-19 04:29:41 -0300

    Added sett

---
+++ lua/entities/lol_sett.lua
@@ -0,0 +1,14 @@
+AddCSLuaFile()
+
+ENT.Base = "lol_champ"
+
+ENT.PrintName = "Sett"
+ENT.Category = "League Of Legends"
+
+ENT.Spawnable = true
+ENT.AdminOnly = true
+
+ENT.Model = "models/gonzo/sett.mdl"
+
+function ENT:PostDataTable()
+end
