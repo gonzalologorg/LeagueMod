92a18e5b2a216983b0796ea64a01e3f7a44c62c7
Gonzalolog <gonzalolog@gmail.com>
2023-06-30 19:05:35 -0300

    Updated MainFrame

--- lua/league/core/ui/cl_atlas.lua
+++ lua/league/core/ui/cl_atlas.lua
@@ -1,7 +1,11 @@
 League.Atlas = {
-    HUD = {}
+    HUD = {},
+    Frame = {},
 }
 local header = Material("lol/ui/effects/hudbar")
+local mainhud = Material("lol/ui/mainhud")
+local animbars = Material("lol/ui/effects/bars")
+
 local sizex, sizey = 183, 90
 local curFrame = 1
 for x = 0, 4 do
@@ -11,10 +15,39 @@ for x = 0, 4 do
     end
 end
 
+League.Atlas.Frame = {
+    base = GWEN.CreateTextureNormal(198, 656, 558, 170, mainhud),
+    avatar = GWEN.CreateTextureNormal(0, 656, 166, 170, mainhud),
+    inventory = GWEN.CreateTextureNormal(8, 416, 235, 160, mainhud),
+    shop = {},
+    bars = {},
+    animated = {},
+}
 
+for k = 0, 2 do
+    League.Atlas.Frame.shop[k + 1] = GWEN.CreateTextureNormal(2 + k * 203, 600, 203, 36, mainhud)
+end
+
+local first, barwidth, barheight, gap = 876 / 1024, 462 / 1024, 17.4 / 1024, 4.5 / 1024
+for k = 0, 6 do
+    League.Atlas.Frame.bars[k + 1] = function(x, y, w, h, progress)
+        surface.SetMaterial(mainhud)
+        surface.SetDrawColor(clr or color_white)
+        surface.DrawTexturedRectUV(x, y, w * progress, h, gap / 4, first + k * barheight + gap * k, barwidth * progress, first + k * barheight + k * gap + barheight)
+    end
+end
+
+curFrame = 1
+local start, bh2, bw2 = 465 / 2048, 16 / 2048, 463 / 1024
+League.Atlas.Frame.animated = function(x, y, w, h, progress)
+    local frame = 1 + math.floor(RealTime() * 50) % 50
+    surface.SetMaterial(animbars)
+    surface.SetDrawColor(clr or color_white)
+    local ux, uy =  frame % 2 == 0 and 0 or bw2,
+                    start + (math.ceil(frame / 2) - 1) * bh2
+    surface.DrawTexturedRectUV(x, y, w * progress, h, ux, uy, ux + bw2 * progress, uy + bh2)
+end
 
 hook.Add("HUDPaint", "League.DrawHUD", function()
-    local frame = 1 + math.Round(RealTime() * 10) % 19
-    local x, y = ScrW() / 2, ScrH() / 2
-//    League.Atlas.HUD[frame](x, y, sizex, sizey)
+    //League.Atlas.Frame.animated(96, 96, 462, 18, 1)
 end)
--- lua/league/core/ui/cl_mainframe.lua
+++ lua/league/core/ui/cl_mainframe.lua
@@ -1,2 +1,62 @@
-local a
-MsgN("d")
+local PANEL = {}
+AccessorFunc(PANEL, "m_eChampion", "Champion", FORCE_BOOL)
+local gold = Color(227, 203, 66)
+local w, h = 558 * .8, 170 * .8
+function PANEL:Init()
+    League.FramePanel = self
+
+    self:SetSize(1024, h)
+    self:AlignBottom(0)
+    self:AlignRight(ScrW() / 2 - self:GetWide() / 2)
+
+    self:ShowCloseButton(false)
+    self:SetTitle("")
+    self:SetDraggable(false)
+    self:SetCursor("no")
+
+    self.inventory = vgui.Create("DPanel", self)
+    self.inventory:SetPos(self:GetWide() / 2 + w / 2 - 2, 0)
+    self.inventory:SetSize(236 * .8, 168 * .8)
+    self.inventory.Paint = function(s, w, h)
+        League.Atlas.Frame.inventory(0, 0, w, h)
+    end
+
+    self.ShopButton = vgui.Create("DButton", self.inventory)
+    self.ShopButton:Dock(BOTTOM)
+    self.ShopButton:SetTall(36 * .8)
+    self.ShopButton:DockMargin(8, 0, 8, 8)
+    self.ShopButton:SetText("")
+    self.ShopButton.Paint = function(s, w, h)
+        League.Atlas.Frame.shop[s:IsHovered() and (input.IsMouseDown(MOUSE_LEFT) and 2 or 3) or 1](0, 0, w, h)
+        draw.SimpleTextOutlined(IsValid(self:GetChampion()) and self:GetChampion():GetGold() or 400, League:Font(22), w / 2, h / 2, gold, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, color_black)
+    end
+end
+
+local icon = Material("lol/champions/sett/avatar")
+
+function PANEL:Paint(rw, rh)
+    //surface.SetDrawColor(255, 0, 0)
+    //surface.DrawRect(0, 0, rw, rh)
+
+    League.Atlas.Frame.base(rw / 2 - w / 2, 0, w, h)
+
+    surface.SetMaterial(icon)
+    surface.SetDrawColor(255, 255, 255)
+    surface.DrawTexturedRectRotated(rw / 2 - w / 2 - 16, h / 2, h * .8, h * .8, 0)
+    League.Atlas.Frame.avatar(rw / 2 - w / 2 - 76, 6, 166 * .8, h)
+
+    League.Atlas.Frame.bars[3](rw / 2 - w / 2 + 54, h - 46, 373, 16, .75)
+    League.Atlas.Frame.animated(rw / 2 - w / 2 + 54, h - 46, 373, 16, .75)
+    League.Atlas.Frame.bars[1](rw / 2 - w / 2 + 54, h - 46 + 18, 373, 16, 1)
+
+    draw.SimpleTextOutlined(1, League:Font(22), rw / 2 - w / 2 + 12, h - 25, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, color_black)
+    draw.SimpleTextOutlined(LocalPlayer():Health() .. "/" .. LocalPlayer():GetMaxHealth(), League:Font(18), rw / 2, h - 38, color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, color_black)
+end
+
+vgui.Register("lol.hudpanel", PANEL, "DFrame")
+
+if IsValid(League.FramePanel) then
+    League.FramePanel:Remove()
+end
+
+//vgui.Create("lol.hudpanel")
